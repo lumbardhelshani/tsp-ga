dc85992360a3e5db3f5ebd6cc52eeaac49ce1a38
e20191480 <e20191480@novaims.unl.pt>
2020-05-15 13:52:45 +0100

    interface for simulated annealing and hill climbing

---
+++ src/GenticAlgorithm/InitializationApproach.java
@@ -0,0 +1,14 @@
+package GenticAlgorithm;
+
+import Models.Route;
+
+import java.util.List;
+
+/**
+ * @author lumba
+ * @project ga-tsp
+ * @date 5/15/2020, @time 13:50
+ */
+public interface InitializationApproach {
+    public List<Route> population();
+}
