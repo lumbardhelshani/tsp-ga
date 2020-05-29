16dccf393654dda5b8577c7474fe90c9f04b5789
e20191480 <e20191480@novaims.unl.pt>
2020-05-29 15:20:37 +0100

    pre final version 3

---
+++ src/GenticAlgorithm/CrossoverType.java
@@ -0,0 +1,10 @@
+package GenticAlgorithm;
+
+//ENUM that checks which kind of crossover type was choosen by user.
+public enum CrossoverType {
+    PMX,
+    CYCLE;
+
+    private CrossoverType() {
+    }
+}
